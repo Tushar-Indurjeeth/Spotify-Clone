4b98482d67e8de91593df7b04d8ffa1d47b5a104
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2021-03-01 13:45:57 +0200

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    // REMOVE LATER
-    // token: 'BQA_OpT9nSN5uCy409KsaXCZCLXs30uGarbVL5XS81v-mKQSWkAtkeK_ZL0U0ma6CBt9897RgFjPOYafS5FM7C0AegrC7ZJQbBc0Idw77kxwUZUKhyuvzArb8ejlp8ALBz7pwt3d4FnJ2M3fEmEVwPhOAvWltyelrZsNGsz3Z4OR0BAM--hy',
+    token: null,
 };
 
 const reducer = (state, action) => {
